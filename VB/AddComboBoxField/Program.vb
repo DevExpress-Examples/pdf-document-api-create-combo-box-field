730f4d8f3eaec02a575982b25ca477a2650dd36b
DevExpressExampleBot <fakeEmail>
2022-02-02 15:24:13 +0300

    Source auto update [skip ci]

--- VB/AddComboBoxField/Program.vb
+++ VB/AddComboBoxField/Program.vb
@@ -1,19 +1,17 @@
-﻿Imports DevExpress.Pdf
+Imports DevExpress.Pdf
 Imports System.Drawing
 
-
 Namespace AddComboBoxField
-    Friend Class Program
-        Shared Sub Main(ByVal args() As String)
-            Using processor As New PdfDocumentProcessor()
 
+    Friend Class Program
+
+        Shared Sub Main(ByVal args As String())
+            Using processor As PdfDocumentProcessor = New PdfDocumentProcessor()
                 ' Create an empty document. 
                 processor.CreateEmptyDocument("..\..\Result.pdf")
-
                 ' Create graphics and draw a combo box field.
                 Using graphics As PdfGraphics = processor.CreateGraphics()
                     DrawComboBoxField(graphics)
-
                     ' Render a page with graphics.
                     processor.RenderNewPage(PdfPaperSize.Letter, graphics)
                 End Using
@@ -21,22 +19,18 @@ Namespace AddComboBoxField
         End Sub
 
         Private Shared Sub DrawComboBoxField(ByVal graphics As PdfGraphics)
-
             ' Create a combo box field specifying its name and location.
-            Dim comboBox As New PdfGraphicsAcroFormComboBoxField("combo Box", New RectangleF(20, 20, 100, 20))
-
+            Dim comboBox As PdfGraphicsAcroFormComboBoxField = New PdfGraphicsAcroFormComboBoxField("combo Box", New RectangleF(20, 20, 100, 20))
             ' Add values to the combo box.  
             comboBox.AddValue("Red")
             comboBox.AddValue("Yellow")
             comboBox.AddValue("Green")
             comboBox.AddValue("Blue")
-
             ' Specify combo box selected value, text allignment, and appearance.
             comboBox.SelectValue("Red")
             comboBox.TextAlignment = PdfAcroFormStringAlignment.Far
             comboBox.Appearance.BackgroundColor = Color.Beige
             comboBox.Appearance.FontSize = 14
-
             ' Add the field to the document.
             graphics.AddFormField(comboBox)
         End Sub
